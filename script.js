d36043777e6671e4a7731caf9531d20aa520ac6b
Tan Sheng Da <tanshengda@Tans-MacBook-Pro.local>
2022-08-25 22:34:17 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const pokemonName = document.getElementById('pokemonName');
+const id = document.getElementById('id');
+const button = document.querySelector( '.button');
+
+button.addEventListener("click", (e) => {
+    e.preventDefault()
+    pokemonName.innerHTML = '<em>Loading...</em>'
+    id.innerHTML = '<em>Loading...</em>'
+
+    const randomNumber = Math.ceil(Math.random() * 905);
+    // console.log(randomNumber);
+    fetch(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`)
+    .then((response) => response.json())
+    .then((data) => {
+    //   console.log(data['name']);
+    //   console.log(data['id']);
+      pokemonName.innerText = data['name'];
+      id.innerText = data['id'];
+    });
+  
+});
